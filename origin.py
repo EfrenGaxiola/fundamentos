4cdb7945543bb37122980ae69a27df5ed0283708
Efren Gaxiola <efren.gaxiola123@gmail.com>
2021-10-04 20:24:18 -0700

    Cadenas de python

---
+++ origin.py
@@ -0,0 +1,15 @@
+name ="jesus efren gaxiola herrera"
+age=10
+istall=True
+height=1.89
+print (name +" tiene "+str(age)+" años")
+print("{} tiene {} años".format(name,age))
+
+print(f"{name} tiene {age+10} años es \
+alto { istall} y mide {height}")
+
+print(name.upper())
+print(name.lower())
+print(name.title())
+
+print(name*5)
