5625011f8ba047e6eb10fd6408a0d5a8ef607510
Efren Gaxiola <efren.gaxiola123@gmail.com>
2021-10-04 20:53:36 -0700

    modulos de python

---
+++ main.py
@@ -0,0 +1,9 @@
+#import operations
+from operations import *
+print (sum(10,5))
+print (rest(10,5))
+print(mensaje)
+#print(operations.sum(10, 5))
+#print(operations.mult(10, 5))
+
+#sets, list, ciclo for y while
